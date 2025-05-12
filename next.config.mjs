2b355186f480887363374b3fab46a8977dc5cd13
Amine Nadi <amine@alphalab72.com>
2025-05-12 02:49:50 +0100

    Add Vercel Blob domain to Next.js image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     productionBrowserSourceMaps: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '*.public.blob.vercel-storage.com',
+          pathname: '/**',
+        },
+      ],
+    },
   };
   
   export default nextConfig;
-  
